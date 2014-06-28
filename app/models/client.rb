c2b3f67a3146dfa71c6b308697b1dcad459f641a
Chris Yancey <cyancey1@gmail.com>
2014-06-27 20:35:26 -0700

    added presence validations for client attributes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ActiveRecord::Base
   belongs_to :user
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :salesforce_id, presence: true
 end
