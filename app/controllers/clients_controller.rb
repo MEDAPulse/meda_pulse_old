e3e3065adced0c4354092e5c8448cc6786d8f5a4
Chris Yancey <cyancey1@gmail.com>
2014-06-27 20:18:47 -0700

    implemented logic in clients#create action to add new client from form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,12 @@
 class ClientsController < ApplicationController
   def create
+    @client = Client.new(client_params)
+    @client.user = current_user
+    if @client && @client.save
+      redirect_to dashboard_path
+    else
+      flash[:errors] = @client.errors.full_messages
+    end
   end
 
   def destroy
@@ -9,5 +16,11 @@ class ClientsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:first_name, :last_name, :salesforce_id, :phone, :email)
   end
 end
