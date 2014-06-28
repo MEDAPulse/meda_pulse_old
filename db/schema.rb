9e782c2629ea6c4050328abdde7dcd4083e8a33a
Chris Yancey <cyancey1@gmail.com>
2014-06-27 22:32:43 -0700

    created migration for goals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628052354) do
+ActiveRecord::Schema.define(version: 20140628052932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140628052354) do
     t.string   "phone",         null: false
     t.string   "salesforce_id", null: false
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "goals", force: true do |t|
+    t.integer  "action_plan_id"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
