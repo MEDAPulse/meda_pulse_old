5b67e7ba394cb3f5a350fd1d4d2ec0904b8af60e
Chris Yancey <cyancey1@gmail.com>
2014-06-27 23:24:55 -0700

    created seed file which makes clients, action plans, goals and steps for the last user created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Client.destroy_all
+ActionPlan.destroy_all
+Goal.destroy_all
+Step.destroy_all
+
+def create_client
+  client = Client.create(
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name,
+                phone: Faker::PhoneNumber.phone_number,
+                salesforce_id: Faker::Company.duns_number,
+                email: Faker::Internet.email,
+                user: User.last
+                )
+  create_action_plan(client)
+end
+
+def create_action_plan(client)
+  action_plan = ActionPlan.create(
+      description: Faker::Company.catch_phrase,
+      client: client
+    )
+  create_goals(action_plan, rand(5)+1)
+end
+
+def create_goals(action_plan, number_to_create)
+  number_to_create.times do
+    goal = Goal.create(
+        description: Faker::Company.catch_phrase,
+        action_plan: action_plan
+      )
+    create_steps(goal, rand(5)+1)
+  end
+end
+
+def create_steps(goal, number_to_create)
+  number_to_create.times do
+    step = Step.create(
+        description: Faker::Company.bs,
+        complete: [true, false].sample,
+        due_by: Date.today+rand(365),
+        goal: goal
+      )
+  end
+end
+
+10.times do
+  create_client
+end
