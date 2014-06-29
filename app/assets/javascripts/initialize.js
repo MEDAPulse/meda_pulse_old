ba214c1928437b058523d4c5bec210485381f0b0
Chris Yancey <cyancey1@gmail.com>
2014-06-29 15:08:57 -0700

    added links and logic to show an action plans goals, a goals steps and both similtaneously

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -4,10 +4,53 @@ $(document).ready(function(){
 
 function setListeners(){
   $('body').on('click', '#show-client-form', toggleAddClientForm)
+  $('body').on('click', '.show-steps', showStepsLink)
+  $('body').on('click', '.show-goals', showGoalsLink)
+  $('body').on('click', '.show-goals-steps', showGoalsAndStepsLink)
 }
 
 function toggleAddClientForm(e){
   e.preventDefault()
   var addClientForm = document.getElementById('new_client')
   addClientForm.classList.toggle('hidden')
+}
+
+function showStepsLink(e){
+  e.preventDefault()
+  var goalId = this.dataset.goalId
+  var goalDiv = document.getElementById('goal-'+goalId)
+  showSteps(goalDiv)
+}
+
+function showSteps(goalDiv){
+  var goalSteps = goalDiv.querySelector('.goal-steps')
+  goalSteps.classList.remove('hidden')
+}
+
+function showGoalsLink(e){
+  e.preventDefault()
+  var actionPlanId = this.dataset.actionPlanId
+  var actionPlanDiv = document.getElementById('action-plan-'+actionPlanId)
+  showGoals(actionPlanDiv)
+}
+
+function showGoals(actionPlanDiv){
+  var actionPlanGoals = actionPlanDiv.querySelector('.action-plan-goals')
+  actionPlanGoals.classList.remove('hidden')
+}
+
+function showGoalsAndStepsLink(e){
+  e.preventDefault()
+  var actionPlanId = this.dataset.actionPlanId
+  var actionPlanDiv = document.getElementById('action-plan-'+actionPlanId)
+  showGoalsAndSteps(actionPlanDiv)
+}
+
+function showGoalsAndSteps(actionPlanDiv) {
+  showGoals(actionPlanDiv)
+  var goalDivs = actionPlanDiv.getElementsByClassName('goal')
+  numberOfGoals = goalDivs.length
+  for(var i=0; i<numberOfGoals; i++){
+    showSteps(goalDivs[i])
+  }
 }
