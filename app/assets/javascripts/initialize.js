b253b442f31f1b5031fd0e191dc2058893e410a5
Chris Yancey <cyancey1@gmail.com>
2014-06-29 16:23:35 -0700

    able to show/hide action plan goals/steps for client

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -7,6 +7,8 @@ function setListeners(){
   $('body').on('click', '.show-steps', showStepsLink)
   $('body').on('click', '.show-goals', showGoalsLink)
   $('body').on('click', '.show-goals-steps', showGoalsAndStepsLink)
+  $('body').on('click', '.hide-steps', hideStepsLink)
+  $('body').on('click', '.hide-goals', hideGoalsLink)
 }
 
 function toggleAddClientForm(e){
@@ -23,8 +25,40 @@ function showStepsLink(e){
 }
 
 function showSteps(goalDiv){
+  var showStepsLinkElement = goalDiv.querySelector('.show-steps')
+  changeLinkToHideSteps(showStepsLinkElement)
   var goalSteps = goalDiv.querySelector('.goal-steps')
   goalSteps.classList.remove('hidden')
+}
+
+function hideStepsLink(e){
+  e.preventDefault()
+  var goalId = this.dataset.goalId
+  var goalDiv = document.getElementById('goal-'+goalId)
+  hideSteps(goalDiv)
+}
+
+function hideSteps(goalDiv){
+  var hideStepsLinkElement = goalDiv.querySelector('.hide-steps')
+  changeLinkToShowSteps(hideStepsLinkElement)
+  var goalSteps = goalDiv.querySelector('.goal-steps')
+  goalSteps.classList.add('hidden')
+}
+
+function changeLinkToHideSteps(showStepsLinkElement){
+  if(showStepsLinkElement) {
+    showStepsLinkElement.classList.remove('show-steps')
+    showStepsLinkElement.classList.add('hide-steps')
+    showStepsLinkElement.innerText = "Hide Steps"
+  }
+}
+
+function changeLinkToShowSteps(hideStepsLinkElement){
+  if(hideStepsLinkElement) {
+    hideStepsLinkElement.classList.remove('hide-steps')
+    hideStepsLinkElement.classList.add('show-steps')
+    hideStepsLinkElement.innerText = "Show Steps"
+  }
 }
 
 function showGoalsLink(e){
@@ -35,6 +69,8 @@ function showGoalsLink(e){
 }
 
 function showGoals(actionPlanDiv){
+  var showGoalsLinkElement = actionPlanDiv.querySelector('.show-goals')
+  changeShowGoalsLinkToHideGoals(showGoalsLinkElement)
   var actionPlanGoals = actionPlanDiv.querySelector('.action-plan-goals')
   actionPlanGoals.classList.remove('hidden')
 }
@@ -52,5 +88,40 @@ function showGoalsAndSteps(actionPlanDiv) {
   numberOfGoals = goalDivs.length
   for(var i=0; i<numberOfGoals; i++){
     showSteps(goalDivs[i])
+  }
+}
+
+function changeShowGoalsLinkToHideGoals(showGoalsLinkElement) {
+  if (showGoalsLinkElement) {
+    showGoalsLinkElement.classList.remove('show-goals')
+    showGoalsLinkElement.classList.add('hide-goals')
+    showGoalsLinkElement.innerText = 'Hide Goals'
+  }
+}
+
+function changeHideGoalsLinkToShowGoals(hideGoalsLinkElement) {
+  hideGoalsLinkElement.classList.add('show-goals')
+  hideGoalsLinkElement.classList.remove('hide-goals')
+  hideGoalsLinkElement.innerText = 'Show Goals'
+}
+
+function hideGoalsLink(e){
+  e.preventDefault()
+  var actionPlanId = this.dataset.actionPlanId
+  var actionPlanDiv = document.getElementById('action-plan-'+actionPlanId)
+  hideGoalsAndSteps(actionPlanDiv)
+}
+
+
+
+function hideGoalsAndSteps(actionPlanDiv){
+  var hideGoalsLinkElement = actionPlanDiv.querySelector('.hide-goals')
+  changeHideGoalsLinkToShowGoals(hideGoalsLinkElement)
+  var actionPlanGoals = actionPlanDiv.querySelector('.action-plan-goals')
+  actionPlanGoals.classList.add('hidden')
+  var goalDivs = actionPlanDiv.getElementsByClassName('goal')
+  numberOfGoals = goalDivs.length
+  for(var i=0; i<numberOfGoals; i++){
+    hideSteps(goalDivs[i])
   }
 }
