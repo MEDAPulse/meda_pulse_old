ca8a3995ce872a28f5c82a93f5b0e6b4a46b84b3
Chris Yancey <cyancey1@gmail.com>
2014-07-02 00:37:32 -0700

    added delete dependent associations between action plans and goals and goals and steps

--- app/models/action_plan.rb
+++ app/models/action_plan.rb
@@ -1,4 +1,4 @@
 class ActionPlan < ActiveRecord::Base
   belongs_to :client
-  has_many :goals
+  has_many :goals, dependent: :destroy
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,4 @@
 class Goal < ActiveRecord::Base
   belongs_to :action_plan
-  has_many :steps
+  has_many :steps, dependent: :destroy
 end
