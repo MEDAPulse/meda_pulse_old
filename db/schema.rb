52d418660165dc5556e14883e6d3d801d5a8eb8c
Chris Yancey <cyancey1@gmail.com>
2014-07-10 18:51:12 -0700

    updated db schema after performing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628053654) do
+ActiveRecord::Schema.define(version: 20140711001922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20140628053654) do
     t.text     "description"
     t.date     "due_by"
     t.boolean  "complete",    default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "text_messages", force: true do |t|
+    t.integer  "client_id"
+    t.text     "content"
+    t.boolean  "incoming_message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
